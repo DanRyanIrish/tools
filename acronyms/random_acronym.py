dff8ea658914bbd1ca2d8439c078817bac2202b9
DanRyanIrish <ryand5@tcd.ie>
2020-06-15 15:36:25 -0400

    Add ability for user to define delimiter and acronym column.

--- acronyms/random_acronym.py
+++ acronyms/random_acronym.py
@@ -19,42 +19,65 @@ def input_file(file_name):
 
 
 def check_input():
-    """ Checking for exit command from user"""
+    """Checking for exit command from user"""
     user_input = input("")
     if user_input == 'c':
         return False
     return True
 
 
-def random_acronym(acronyms):
+def random_acronym(acronyms, acronym_col, delimiter):
     """
     Outputs random acronym read in from txt file.
+
     On user keyboard input, meaning of acronym is printed.
+
+    Parameters
+    ----------
+    acronyms: `list` of `str`
+        Each element gives the acronym, its meaning and other details separated by a delimiter.
+
+    acronym_col: `int`
+        The column in each line where the acronym with which to prompt the user can be found.
+
+    delimiter: `str`
+        The delimiter character that separates the columns in each line.
     """
     user_input = True
     print("File has {0} acronyms.".format(len(acronyms)))
     while user_input:
-        acronym = random.choice(acronyms)
-        acronym_and_meaning = acronym.split(" - ")
-        print(acronym_and_meaning[0], end='')
-        input("")
-        print(acronym)
+        line = random.choice(acronyms)
+        line = line.split(delimiter)
+        acronym = line.pop(acronym_col)
+        meaning = " - ".join(line)
+        print(acronym, end=': ')
+        input()
+        print(meaning)
         user_input = check_input()
 
 
 def parse_args():
-    """ Parses command line arguements """
+    """Parses command line arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-f", "--file_name",
         type=str, help="File of acronyms to be read in."
     )
+    parser.add_argument(
+            "-d", "--delimiter",
+            type=str,
+            help="Delimiter separating terms and definitions in file lines.",
+            default=" - ")
+    parser.add_argument(
+            "-s", "--show_column",
+            type=int,
+            help="The column to show the user first.",
+            default=0)
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     args = parse_args()
-    file_name = args.file_name
-    acronyms = input_file(file_name)
-    random_acronym(acronyms)
+    acronyms = input_file(args.file_name)
+    random_acronym(acronyms, args.show_column, args.delimiter)
